0416fa904048c4ec4eebdd00175046c42f56d485
Edward Shaw <edwardshaw9@gmail.com>
2018-03-10 16:00:54 +0000

    Build on MacOS

--- build.rs
+++ build.rs
@@ -10,19 +10,23 @@ fn main() {
     lib_dir.push(target.clone());
     println!("cargo:rustc-link-search=all={}", lib_dir.display());
 
-    let mut dll_dir = manifest_dir.clone();
-    dll_dir.push("dll");
-    dll_dir.push(target);        
-    for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
-        let entry_path = entry.expect("Invalid fs entry").path();
-        let file_name_result = entry_path.file_name();
-        let mut new_file_path = manifest_dir.clone();
-        if let Some(file_name) = file_name_result {
-            let file_name = file_name.to_str().unwrap();
-            if file_name.ends_with(".dll") {
-                new_file_path.push(file_name);
-                std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+    let mut dll_dir_path = manifest_dir.clone();
+    dll_dir_path.push("dll");
+    dll_dir_path.push(target);
+
+    if let Ok(dll_dir) = std::fs::read_dir(dll_dir_path) {
+        for entry in dll_dir {
+            let entry_path = entry.expect("Invalid fs entry").path();
+            let file_name_result = entry_path.file_name();
+            let mut new_file_path = manifest_dir.clone();
+            if let Some(file_name) = file_name_result {
+                let file_name = file_name.to_str().unwrap();
+                if file_name.ends_with(".dll") {
+                    new_file_path.push(file_name);
+                    std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+                }
             }
         }
     }
+
 }
